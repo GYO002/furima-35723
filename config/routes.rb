10e324773fc42bd0ee57a95dcf99891c1c38f6fe
GYO002 <83623654+GYO002@users.noreply.github.com>
2021-06-02 22:29:21 +0900

    usersモデル・テーブル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users   #ユーザー機能に必要な複数のルーティングを一度に生成してくれるdeviseのメソッド
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   resources :items, only: :index
