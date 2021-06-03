51a3c16d9b00791f9a36bc9298af6e6c477388c8
GYO002 <83623654+GYO002@users.noreply.github.com>
2021-06-03 12:59:25 +0900

    新規登録機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :, presence: true
+         
          validates :nickname, presence: true
          validates :last_name, presence: true
          validates :first_name, presence: true
          validates :last_name_ruby, presence: true
          validates :first_name_ruby, presence: true
          validates :first_name_ruby, presence: true
+         validates :birthday, presence: true
 end
