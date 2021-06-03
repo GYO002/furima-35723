a4a911fde8e42ed1a962afda0994e58601582093
GYO002 <83623654+GYO002@users.noreply.github.com>
2021-06-03 21:38:57 +0900

    正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
+         #ニックネームと誕生日のバリデーション
          validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_ruby, presence: true
-         validates :first_name_ruby, presence: true
-         validates :first_name_ruby, presence: true
          validates :birthday, presence: true
+ 
+        #名前のバリテーション（姓名）
+        with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+          validates :first_name
+          validates :last_name
+        end
+
+       #名前のバリテーション（カナ）
+        with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角(カタカナ）を使用してください' } do
+          validates :first_name_ruby
+          validates :last_name_ruby
+        end
+
+        #パスワードのバリデーション
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
 end
