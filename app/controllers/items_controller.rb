c512fc15c2bc12fc7323e5dd6c77bea9b09e7fe9
GYO002 <83623654+GYO002@users.noreply.github.com>
2021-06-08 17:01:43 +0900

    JS前

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,13 +2,28 @@ class ItemsController < ApplicationController
   
   
   def index
+    #@items = Item.includes(:user)
   end
 
   def new
+    @iteme = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product_name, :description, :category_id, :status_id, :burden_id, :area_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,41 @@
 class Item < ApplicationRecord
-end
+  belongs_to :user
+  has_one_attached :image
+  has_many :category
+
+  validates :product_name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :area_id, presence: true
+  validates :delivery_day_id, presence: true
+  validates :price, presence: true
+
+  #【商品出品バリデーション一覧】
+  #カテゴリーのバリデーション（空だと保存できない&「--」を選択すると保存できない）
+  validates :category, presence: true
+  validates :categoru_id, numericality: { other_than: 1 }
+
+  #商品の状態（status）のバリデーション（空だと保存できない&「--」を選択すると保存できない）
+  validates :status, presence: true
+  validates :status_id, numericality: { other_than: 1 }
+  
+  #配送料の負担（burden）のバリデーション（空だと保存できない&「--」を選択すると保存できない）
+  validates :burden, presence: true
+  validates :burden_id, numericality: { other_than: 1 }
+
+  #配送先地域（area）のバリデーション（空だと保存できない&「--」を選択すると保存できない）
+  validates :area, presence: true
+  validates :area_id, numericality: { other_than: 1 }
+
+  #配送日数（derivery）のバリデーション（空だと保存できない&「--」を選択すると保存できない）
+  validates :delivery, presence: true
+  validates :delivery_id, numericality: { other_than: 1 }
+
+  # 価格の
+  validates_inclusion_of :price, in: 300..9999999, message: "半角¥300~¥9,999,999の間でお願いします"
+  #with_options presence: true, format: { with: /(/\d{3,7}/)/, message: '半角¥300~¥9,999,999の間でお願いします' } do
+    #validates :price
+  #end
+
+  end
