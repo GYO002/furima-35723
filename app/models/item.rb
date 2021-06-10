9dafa8f56df1fa048921df85601f10843d2bf8ff
GYO002 <83623654+GYO002@users.noreply.github.com>
2021-06-10 13:38:42 +0900

    商品出品機能　修正１

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,40 +8,30 @@ class Item < ApplicationRecord
   belongs_to :delivery
   belongs_to :status
 
-  validates :product_name, presence: true
-  validates :description, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :area_id, presence: true
-  validates :delivery_day_id, presence: true
-  validates :price, presence: true
-
-  #【商品出品バリデーション一覧】
-  #カテゴリーのバリデーション（空だと保存できない&「--」を選択すると保存できない）
-  validates :category, presence: true
-  validates :category_id, numericality: { other_than: 1 }
-
-  #商品の状態（status）のバリデーション（空だと保存できない&「--」を選択すると保存できない）
-  validates :status, presence: true
-  validates :status_id, numericality: { other_than: 1 }
   
-  #配送料の負担（burden）のバリデーション（空だと保存できない&「--」を選択すると保存できない）
-  validates :burden, presence: true
-  validates :burden_id, numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :product_name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :area_id
+    validates :delivery_day_id
+    validates :price
+  end
 
-  #配送先地域（area）のバリデーション（空だと保存できない&「--」を選択すると保存できない）
-  validates :area, presence: true
+  with_options numericality: { other_than: 1 } do
+    validates :product_name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :area_id
+    validates :delivery_day_id
+    validates :price
+  end
+
   validates :area_id, numericality: { other_than: 0 }
 
-  #配送日数（derivery）のバリデーション（空だと保存できない&「--」を選択すると保存できない）
-  validates :delivery_day_id, presence: true
-  validates :delivery_day_id, numericality: { other_than: 1 }
-
-  # 価格の
   validates_inclusion_of :price, in: 300..9999999, message: "半角¥300~¥9,999,999の間でお願いします"
-  #with_options presence: true, format: { with: /(/\d/)/, message: '半角¥300~¥9,999,999の間でお願いします' } do
   validates :price, numericality: { only_integer: true }
-    #validates :price
-  #end
 
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Item, type: :model do
    end
   end
   
-  context '商品出品ができる時' do
+  context '商品出品ができない時' do
     it '商品画像を１枚つけることが必須であること' do
       @item.image = nil
       @item.valid?
@@ -41,25 +41,41 @@ RSpec.describe Item, type: :model do
 
 
     it '商品の状態についての情報が必須であること' do
-      @item.status_id=
+      @item.status_id=''
       @item.valid?
       expect(@item.errors.full_messages).to include "User must exist"
     end
 
     it '配送料の負担についての情報が必須であること' do
-      @item.burden_id=
+      @item.burden_id=''
       @item.valid?
       expect(@item.errors.full_messages).to include "User must exist"
     end
 
     it '配送元の地域の情報が必須であること' do
-      @item.area_id=
+      @item.area_id=''
       @item.valid?
       expect(@item.errors.full_messages).to include "User must exist"
     end
 
     it '配送までの日数についての情報が必須であること' do
-      @item.delivery_day_id=
+      @item.delivery_day_id=''
+      @item.valid?
+      expect(@item.errors.full_messages).to include "User must exist"
+    end
+
+    it 'idに1が選択されている場合は出品できない' do
+      item = Item.new(category_id: 1)
+      item = Item.new(status_id: 1)
+      item = Item.new(burden_id: 1)
+      item = Item.new(delivery_day_id: 1)
+      @item.valid?
+      expect(@item.errors.full_messages).to include "User must exist"
+    end
+
+
+    it '(配送先地域の（area)場合はidが0の時は出品できない)' do
+      item = Item.new(area_id: 0)
       @item.valid?
       expect(@item.errors.full_messages).to include "User must exist"
     end
@@ -70,14 +86,32 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include "Price 半角¥300~¥9,999,999の間でお願いします"
     end
 
-    it '販売価格は、¥300~¥9,999,999の間のみ保存可能であること' do
+    it '商品価格が299円以下では出品できない' do
       item = Item.new(price: 200)
+      item.valid?
+      expect(item.errors.full_messages).to include "Price 半角¥300~¥9,999,999の間でお願いします"
+    end
+
+    it '商品価格が10_000_000円以上では出品できない' do
+      item = Item.new(price: 100000000)
       item.valid?
       expect(item.errors.full_messages).to include "Price 半角¥300~¥9,999,999の間でお願いします"
     end
 
     it '販売価格は半角数字のみ保存可能であること' do
       item = Item.new(price: '１００')
+      item.valid?
+      expect(item.errors.full_messages).to include "Price 半角¥300~¥9,999,999の間でお願いします"
+    end
+
+    it '商品価格が半角英数字混合では出品できない' do
+      item = Item.new(price: '111aaa')
+      item.valid?
+      expect(item.errors.full_messages).to include "Price 半角¥300~¥9,999,999の間でお願いします"
+    end
+
+    it '商品価格が半角英字のみでは出品できない' do
+      item = Item.new(price: 'aaaaa')
       item.valid?
       expect(item.errors.full_messages).to include "Price 半角¥300~¥9,999,999の間でお願いします"
     end
