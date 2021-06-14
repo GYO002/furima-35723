96f556baee28e8bd85e52a1959654230be2daff7
GYO002 <83623654+GYO002@users.noreply.github.com>
2021-06-14 19:17:53 +0900

    商品削除実装２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,9 +37,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
 
     
-
   private
 
   def item_params
